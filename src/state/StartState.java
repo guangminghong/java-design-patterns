f7dcb4f5c29d4a12e7a796e9862834d7bd8728c6
jinlong <g_jinlong@163.com>
2016-10-16 23:36:52 +0800

    add state pattern

---
+++ src/state/StartState.java
@@ -0,0 +1,17 @@
+package state;
+
+
+public class StartState implements State{
+
+	@Override
+	public void doAction(Context context) {
+		System.out.println("Player is in start state");
+		context.setState(this);
+	}
+
+	@Override
+	public String toString() {
+		return "StartState ";
+	}
+
+}
---
+++ src/state/StopState.java
@@ -0,0 +1,16 @@
+package state;
+
+public class StopState implements State{
+
+	@Override
+	public void doAction(Context context) {
+		System.out.println("Player is in stop state");
+		context.setState(this);
+	}
+	
+	@Override
+	public String toString() {
+		return "StopState";
+	}
+
+}
