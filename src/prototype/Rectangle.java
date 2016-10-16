b473ad92f50cc5608a8c3a3c84adccf181623c34
jinlong <g_jinlong@163.com>
2016-10-16 22:56:06 +0800

    add prototype pattern

---
+++ src/prototype/Rectangle.java
@@ -0,0 +1,13 @@
+package prototype;
+
+public class Rectangle extends Shape{
+	public Rectangle() {
+		type = "Rectangle";
+	}
+	@Override
+	void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Rectangle::draw() method.");
+	}
+
+}
---
+++ src/prototype/Square.java
@@ -0,0 +1,13 @@
+package prototype;
+
+public class Square extends Shape{
+	public Square() {
+		type = "Square";
+	}
+	@Override
+	void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Square::draw() method.");
+	}
+
+}
