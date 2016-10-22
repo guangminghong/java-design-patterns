53fc8c431c77ce7dd1b77779c6af4b8b0318cde6
jinlong <g_jinlong@163.com>
2016-10-22 22:47:54 +0800

    rename file name

--- src/template/TemplatePatternDemo.java
+++ src/template/TemplatePatternDemo.java
@@ -3,7 +3,7 @@ package template;
 /**
  * Created by jinlong on 2016/10/22.
  */
-public class TmplatePatternDemo {
+public class TemplatePatternDemo {
     public static void main(String[] args) {
         Game game = new Cricket();
         game.play();
